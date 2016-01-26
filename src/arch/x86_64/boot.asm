c49950fc624317d8b84140336f1bd0dd03c4e752
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-01-26 20:18:20 +0100

    Setup labels and make a long jump to 64-bit code

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,6 +1,7 @@
 ;;; Copied from http://blog.phil-opp.com/rust-os/multiboot-kernel.html
 
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -16,6 +17,14 @@ start:
 
     ; load the 64-bit GDT
     lgdt [gdt64.pointer]
+
+    ; update selectors
+    mov ax, gdt64.data
+    mov ss, ax ; stack selector
+    mov ds, ax ; data selector
+    mov es, ax ; extra selector
+
+    jmp gdt64.code:long_mode_start
 
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
@@ -153,7 +162,9 @@ stack_top:
 section .rodata
 gdt64:
     dq 0 ; zero entry
+.code: equ $ - gdt64
     dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+.data: equ $ - gdt64
     dq (1<<44) | (1<<47) | (1<<41) ; data segment
 .pointer:
     dw $ - gdt64 - 1
