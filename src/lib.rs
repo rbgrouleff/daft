5a57a147c3774aa61f3ac9c88fe396ee22af5588
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-01-31 19:09:48 +0100

    Enable SSE

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ extern crate rlibc;
 pub extern fn rust_main() {
     let x = ["Hello", " ", "World", "!"];
     let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    let mut a = ("hello", 42);
+    a.1 += 1;
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
