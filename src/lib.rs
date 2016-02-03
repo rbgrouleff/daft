4982b193313c8879a17513891957cc79fc8ecb08
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-03 23:10:29 +0100

    Add features, the vga_buffer module and attribution

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+// Copied from http://blog.phil-opp.com/rust-os
+
 #![feature(lang_items)]
+#![feature(const_fn, unique)]
 #![no_std]
 
 extern crate rlibc;
+
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
