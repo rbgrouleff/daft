acb42fca1c827eba18501aa552b99f2d769d13d1
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-17 22:35:19 +0100

    Fetch memory info from the multiboot struct and print it

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,24 @@
 
 extern crate rlibc;
 extern crate spin;
+extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_information_address: usize) {
     // ATTENTION: we have a very small stack and no guard page
     vga_buffer::clear_screen();
     println!("Hello world{}", "!");
+
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
     loop{}
 }
 
