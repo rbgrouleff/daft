912e4430b2a74d383ab48a9129a194aab2a4a508
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-17 22:50:20 +0100

    Print the kernel elf sections

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,15 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     for area in memory_map_tag.memory_areas() {
         println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
     }
+
+    let elf_sections_tag = boot_info.elf_sections_tag().expect("Elf-sections tag required");
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+                 section.addr, section.size, section.flags);
+    }
+
     loop{}
 }
 
