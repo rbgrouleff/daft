a15ec21d13063fca03e4ae5388a2b2d81ccc2153
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-17 22:43:33 +0100

    Implementing panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,9 @@ pub extern fn rust_main(multiboot_information_address: usize) {
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
+#[lang = "panic_fmt"]
+extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}
