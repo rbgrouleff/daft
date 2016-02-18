18f434c53ab9a951cf8c14218d856ecf5dcaa700
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-18 13:49:58 +0100

    Print kernel start/end and multiboot start/end

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,16 @@ pub extern fn rust_main(multiboot_information_address: usize) {
                  section.addr, section.size, section.flags);
     }
 
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+        .min().unwrap();
+    let kernel_end   = elf_sections_tag.sections().map(|s| s.addr + s.size)
+        .max().unwrap();
+    println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end   = multiboot_start + (boot_info.total_size as usize);
+    println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
+
     loop{}
 }
 
