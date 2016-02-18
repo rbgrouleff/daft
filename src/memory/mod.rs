88d4c4f5b549904a8a1dc1a54a156dc203b356b9
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2016-02-18 14:42:29 +0100

    Re-exporting the AreaFrameAllocator

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,3 +1,9 @@
+// Copied from http://blog.phil-opp.com/rust-os
+
+pub use self::area_frame_allocator::AreaFrameAllocator;
+
+mod area_frame_allocator;
+
 pub const PAGE_SIZE: usize = 4096;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
